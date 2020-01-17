e8403c24d620c77497f45abdb15872e022abbe97
sarinarachel <skatz13@uw.edu>
2020-01-17 10:40:03 -0800

    button onCLick added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+
+    <header>
+      <img className="logo"
+        alt="logo"
+        src="https://www.salsify.com/hubfs/Salsify-Chat-Icon.png"
+      />
+      Chatter
+    </header>
+
+    <TextInput onSend={m=> console.log(m)} className="input"/>
+
+  </main>
 }
 
-export default App;
+function TextInput(props){
+  var [text, setText] = useState('')
+  
+  return <div className="text-input-wrap">
+    <input value={text} className="text-input"
+    placeholder="Type your thoughts..."
+    onChange = {e=> setText(e.target.value)}
+    />
+
+    <button onClick={()=> {props.onSend(text)
+    setText('')}}
+    className="send">
+      <img className="send-img"
+        alt="send"
+        src="https://cdn3.iconfinder.com/data/icons/email-51/48/44-512.png"
+        />
+    </button>
+  </div>
+}
+
+export default App
